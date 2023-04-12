8919c5b8f21e3bc1768e1eee3a6dcd95df5340a5
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-12 06:49:26 +0300

    header and navbar style finished

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { HiHome, HiOutlineSearch } from 'react-icons/hi';
+import {
+  HiHome,
+  HiOutlineSearch,
+  HiOutlineUser,
+  HiOutlineMenu,
+} from 'react-icons/hi';
 import { BsPlusCircleDotted } from 'react-icons/bs';
 
 export default function Navbar() {
   return (
-    <div>
-      <div className="fixed bottom-0 w-screen h-12 flex gap-y-4 items-center justify-evenly md:static md:w-12 md:flex-col md:h-fit xl:items-stretch xl:w-full">
+    <>
+      <div className="fixed bottom-0 w-screen h-14 border-t border-gray-300 flex gap-y-4 items-center justify-evenly md:static md:border-none md:mb-5 md:w-12 md:flex-col md:h-full xl:items-stretch xl:w-full">
         <div className="p-2 cursor-pointer hover:bg-gray-100 rounded-full flex gap-x-4 items-center">
           <HiHome size={30} />
           <p className="hidden xl:block font-bold">Home</p>
@@ -18,9 +23,15 @@ export default function Navbar() {
           <BsPlusCircleDotted size={30} />
           <p className="hidden xl:block">Create</p>
         </div>
-        <div className="p-2 cursor-pointer hover:bg-gray-100 rounded-full flex gap-x-4 items-center h-8 w-8 bg-black"></div>
+        <div className="p-2 cursor-pointer hover:bg-gray-100 rounded-full flex gap-x-4 items-center">
+          <HiOutlineUser size={30} />
+          <p className="hidden xl:block">Profile</p>
+        </div>
+        <div className="p-2 cursor-pointer hover:bg-gray-100 rounded-full flex gap-x-4 items-center mt-auto">
+          <HiOutlineMenu size={30} />
+          <p className="hidden xl:block">More</p>
+        </div>
       </div>
-      <div></div>
-    </div>
+    </>
   );
 }
