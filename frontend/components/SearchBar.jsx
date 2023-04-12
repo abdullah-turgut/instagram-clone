447b63b55ccf1a1de9652c95b29cbc393e25714d
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-13 01:36:53 +0300

    Update SearchBar.jsx

--- frontend/components/SearchBar.jsx
+++ frontend/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function SearchBar() {
   return (
@@ -8,13 +8,10 @@ export default function SearchBar() {
         <MagnifyingGlassIcon className="h-1/2 text-gray-500" />
       </div>
       <input
-        type="text"
+        type="search"
         placeholder="Search"
         className="form-input rounded-xl bg-gray-200 border-none pl-10 text-sm focus:ring-0"
       />
-      <div className="top-1/2 -translate-y-1/2 flex items-center absolute right-2 p-1 bg-gray-300 rounded-full h-1/2 text-gray-50 cursor-pointer hover:bg-gray-400">
-        <XMarkIcon className="h-full" />
-      </div>
     </div>
   );
 }
