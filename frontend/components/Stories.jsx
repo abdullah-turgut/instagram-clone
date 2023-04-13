60a33d4043dee9c866e209563cacde07f222ef88
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-13 05:48:02 +0300

    storyUser mocks created

--- frontend/components/Stories.jsx
+++ frontend/components/Stories.jsx
@@ -1,25 +1,9 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Story from './Story';
-import { faker } from '@faker-js/faker/locale/en_GB';
+import { storyUsers } from '@/mocks/storyUsers';
 
 function Stories() {
-  const [storyUsers, setStoryUsers] = useState([]);
-
-  useEffect(() => {
-    for (let i = 0; i < 20; i++) {
-      let user = {
-        userId: faker.datatype.uuid(),
-        username: faker.internet.userName(),
-        avatar: faker.image.avatar(),
-      };
-      storyUsers.push(user);
-    }
-
-    setStoryUsers(storyUsers);
-    console.log(storyUsers);
-  }, []);
-
   return (
     <div className="flex w-full overflow-x-scroll">
       {storyUsers.map((user) => (
---
+++ frontend/mocks/storyUsers.jsx
@@ -0,0 +1,242 @@
+export const storyUsers = [
+  {
+    userId: '61de286b-9c5c-49f7-b526-93a05642dc1d',
+    username: 'ruben_volkman',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/837.jpg',
+  },
+  {
+    userId: '51449955-1d1a-4418-9c12-e0d5a1886886',
+    username: 'wilton11',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/216.jpg',
+  },
+  {
+    userId: 'b3994573-5cdb-4e25-bfc2-7350003643ce',
+    username: 'janessa_bechtelar',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/176.jpg',
+  },
+  {
+    userId: '41a4fcb0-ca7f-40b2-abb9-ab39f1e7fe31',
+    username: 'rudy.runte',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/586.jpg',
+  },
+  {
+    userId: 'a334ba9f-7510-4315-99ac-eb05a97f318c',
+    username: 'dallas34',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/654.jpg',
+  },
+  {
+    userId: 'd37475c7-5920-4d8c-80b5-edc5df2b10ac',
+    username: 'gayle42',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/329.jpg',
+  },
+  {
+    userId: '416470b8-3bc4-4de3-a281-34706bc8156b',
+    username: 'hannah76',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1149.jpg',
+  },
+  {
+    userId: 'c431a247-6cdb-4b87-aad6-0fb472981512',
+    username: 'cecil14',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1041.jpg',
+  },
+  {
+    userId: 'cbbf9993-5f44-4d9b-9c96-5ee08c6c48d5',
+    username: 'bo.kunde',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg',
+  },
+  {
+    userId: 'bee1d893-4a2b-40f2-8ede-3f5952ff758e',
+    username: 'jacky.mitchell80',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1024.jpg',
+  },
+  {
+    userId: '9698d39f-c2bc-4028-93a7-d13ad2285eac',
+    username: 'jermain_jerde92',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/544.jpg',
+  },
+  {
+    userId: '359b6a7b-ccdb-4c08-a0be-e18fa5dd6f74',
+    username: 'novella.cronin66',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/657.jpg',
+  },
+  {
+    userId: 'c1a7e62f-9184-4fc3-b228-a7d663ee713c',
+    username: 'walton_moen69',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/817.jpg',
+  },
+  {
+    userId: 'a708afb4-c9e8-4ed4-a8cd-b561abe6d596',
+    username: 'edison_mccullough96',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/795.jpg',
+  },
+  {
+    userId: 'bf92f2f0-6a18-474b-a7e8-dd14d7fda76c',
+    username: 'fletcher.towne',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/719.jpg',
+  },
+  {
+    userId: '90ea7f25-b2f9-426e-a8ab-a04dee4da17b',
+    username: 'darian.satterfield',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/243.jpg',
+  },
+  {
+    userId: '7dc48f00-f2d0-4f01-a5dc-8520340c0f10',
+    username: 'alexandre_nader54',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/413.jpg',
+  },
+  {
+    userId: 'cbd78b80-ed36-4af9-82d1-c6abd42e1d78',
+    username: 'eve74',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/696.jpg',
+  },
+  {
+    userId: '0f837246-e6c9-4e4f-96b9-af8adf7ffe46',
+    username: 'myron.hayes',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/471.jpg',
+  },
+  {
+    userId: 'f88d83ba-a535-4622-a114-c52ee187ca6b',
+    username: 'queen63',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/238.jpg',
+  },
+  {
+    userId: 'f0d2ca39-5aaf-412a-9985-d6c081a13c87',
+    username: 'rachael_lehner',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/860.jpg',
+  },
+  {
+    userId: 'fa5703d8-2d91-41ed-b4a0-a837743403d3',
+    username: 'eleazar_prohaska12',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/550.jpg',
+  },
+  {
+    userId: '292f9db2-c20b-4120-98ca-a2ed688dc376',
+    username: 'katharina47',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1248.jpg',
+  },
+  {
+    userId: '7c247c04-abd9-4f26-af65-b80ccee05f97',
+    username: 'arlie.fay',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1185.jpg',
+  },
+  {
+    userId: '4c72db9d-c4dc-4d81-8327-6d77421df423',
+    username: 'mose.bergstrom',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/267.jpg',
+  },
+  {
+    userId: '3e74f8ba-30fc-4ced-b4e2-e164a381eb75',
+    username: 'cayla.waters41',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/462.jpg',
+  },
+  {
+    userId: 'acc7cf04-3053-486e-8582-7efd1b6d3002',
+    username: 'darian20',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/232.jpg',
+  },
+  {
+    userId: '243690af-8037-4db8-9468-7807e6f321c7',
+    username: 'rogelio_koss64',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/356.jpg',
+  },
+  {
+    userId: '2abf7319-d858-4722-ab90-eb68f154f9fe',
+    username: 'julie_morar0',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/411.jpg',
+  },
+  {
+    userId: 'f4ad01a8-b98b-472e-904b-6152e862bcf0',
+    username: 'otilia_conroy96',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/527.jpg',
+  },
+  {
+    userId: '26824081-7980-494b-a217-0e47d22aaa38',
+    username: 'katrine.effertz',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/250.jpg',
+  },
+  {
+    userId: '704bdd22-b0b4-4861-bc2b-3efbd3ede5e8',
+    username: 'myrl_bartell8',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/3.jpg',
+  },
+  {
+    userId: '8920680c-49cc-4ec5-aa1e-7044e818aa4f',
+    username: 'grayce78',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/495.jpg',
+  },
+  {
+    userId: '8c8356c9-dffd-4ae7-8184-a62286ee5deb',
+    username: 'chasity50',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/201.jpg',
+  },
+  {
+    userId: '309c1923-5e2f-4a98-9213-d2eaa9cf649d',
+    username: 'tatum95',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/42.jpg',
+  },
+  {
+    userId: '7dd50391-ab07-4914-9ead-4603bb566c81',
+    username: 'carole.turcotte',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/649.jpg',
+  },
+  {
+    userId: '99f5897d-aa46-470b-8131-c26b2f106b21',
+    username: 'summer41',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/424.jpg',
+  },
+  {
+    userId: '96a66ba7-ba24-4be6-a355-4501beaa45be',
+    username: 'oliver_boehm53',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1077.jpg',
+  },
+  {
+    userId: '56eb3083-6542-42ec-aa23-bcd185f5bf66',
+    username: 'braeden_kunde50',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/939.jpg',
+  },
+  {
+    userId: '2f00ba62-4c94-4d8e-9e02-a4c6dd797f88',
+    username: 'mittie41',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1169.jpg',
+  },
+];
