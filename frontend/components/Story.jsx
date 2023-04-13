359af8cd4bfa62f426e2c2a78f9cd1ef490f38a5
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-13 06:16:20 +0300

    Update Story.jsx

--- frontend/components/Story.jsx
+++ frontend/components/Story.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Story({ username, avatar }) {
   return (
-    <div className="flex h-[84px] w-16 flex-col">
+    <div className="flex h-[84px] w-16 flex-col gap-y-1">
       <div className="w-16 h-16 p-[3px] border-2 border-pink-600 rounded-full hover:scale-105 cursor-pointer">
         <img
           src={avatar}
@@ -11,7 +11,7 @@ function Story({ username, avatar }) {
         />
       </div>
 
-      <p className="text-xs truncate ...">{username}</p>
+      <p className="text-xs text-center truncate">{username}</p>
     </div>
   );
 }
