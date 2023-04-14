17508fa7399b7c4efcdf979af05cd0c93effdd3e
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-14 06:20:12 +0300

    post-card style finished

--- frontend/components/Post.jsx
+++ frontend/components/Post.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { HiOutlineDotsHorizontal } from 'react-icons/hi';
+import { BsDot, BsEmojiSmile } from 'react-icons/bs';
+import {
+  FaRegHeart,
+  FaRegComment,
+  FaRegPaperPlane,
+  FaRegBookmark,
+} from 'react-icons/fa';
+import numeral from 'numeral';
 
 function Post({ username, avatar, img, caption }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col pb-5 mb-3 border-b border-gray-300">
       <div className="flex h-14 justify-between px-1 items-center">
-        <div className="flex items-center gap-x-2">
+        <div className="flex items-center gap-x-2 text-sm">
           <div className="w-10 h-10 p-[2px] bg-gradient-to-r to-pink-500 via-red-500 from-yellow-500 rounded-full">
             <img
               src={avatar}
@@ -13,11 +21,70 @@ function Post({ username, avatar, img, caption }) {
               className="w-full rounded-full border-2 border-white"
             />
           </div>
-          <p className="text-sm">{username}</p>
+          <div className="flex items-center">
+            <p>{username}</p>
+            <BsDot size={14} />
+            <p className="text-gray-500">5h</p>
+          </div>
         </div>
-        <HiOutlineDotsHorizontal size={20} />
+        <HiOutlineDotsHorizontal
+          size={20}
+          className="cursor-pointer hover:text-gray-400"
+        />
       </div>
-      <div className="w-full h-[468px] bg-gray-300"></div>
+      <div className="w-full h-[468px]">
+        <img src={img} alt="post-img" className="h-full w-full object-cover" />
+      </div>
+      <div className="w-full h-12 flex justify-between items-center">
+        <div className="flex items-center gap-x-4">
+          <FaRegHeart
+            size={24}
+            className="cursor-pointer hover:text-gray-400"
+          />
+          <FaRegComment
+            size={24}
+            className="cursor-pointer hover:text-gray-400"
+          />
+          <FaRegPaperPlane
+            size={24}
+            className="cursor-pointer hover:text-gray-400"
+          />
+        </div>
+        <FaRegBookmark
+          size={24}
+          className="cursor-pointer hover:text-gray-400"
+        />
+      </div>
+      <div className="flex gap-x-1 text-sm font-medium">
+        <p>{numeral(14214).format('0,0')}</p>
+        <p>likes</p>
+      </div>
+
+      <p className="text-sm mt-1 ">
+        <span className="font-semibold">{username}</span> {caption}
+      </p>
+      <p className="text-sm mt-1 text-gray-500 cursor-pointer hover:underline">
+        View all 12 Comments
+      </p>
+      <form className="w-full mt-1 flex items-center justify-between gap-x-2 relative">
+        <textarea
+          name=""
+          id=""
+          className="w-full max-w-[420px] resize-none border-none align-middle h-5 py-[2px] px-0 text-sm scrollbar-none focus:ring-0"
+          placeholder="Add a comment..."
+        ></textarea>
+        <button
+          type="submit"
+          value="Post"
+          className="bg-transparent text-sky-600 font-medium"
+        >
+          Post
+        </button>
+        <BsEmojiSmile
+          size={13}
+          className="text-gray-600 hover:text-gray-400 cursor-pointer"
+        />
+      </form>
     </div>
   );
 }
--- frontend/components/Posts.jsx
+++ frontend/components/Posts.jsx
@@ -4,7 +4,7 @@ import { postUsers } from '@/mocks/postUsers';
 
 export default function Posts() {
   return (
-    <div className="w-full max-w-[470px] flex flex-col mx-auto">
+    <div className="w-full max-w-[470px] flex flex-col mx-auto mt-4">
       {postUsers.map((post, i) => (
         <Post
           key={i}
