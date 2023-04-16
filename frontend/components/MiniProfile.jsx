68e54cc9956f1c70874cd8e80aeb71b4803cece2
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-16 22:13:14 +0300

    miniprofile styles finished

--- frontend/components/MiniProfile.jsx
+++ frontend/components/MiniProfile.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function MiniProfile() {
-  return <div>MiniProfile</div>;
+  return (
+    <div className="flex justify-between items-center h-14  mt-4 mb-3">
+      <div className="flex items-center h-full gap-x-3">
+        <img
+          src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/176.jpg"
+          alt=""
+          className="h-full rounded-full border border-gray-200"
+        />
+        <div className="flex flex-col text-sm">
+          <p className="font-medium">abdturgut</p>
+          <p className="text-gray-500">Abdullah Turgut</p>
+        </div>
+      </div>
+      <button className="text-sm font-semibold px-2 text-sky-400">
+        Switch
+      </button>
+    </div>
+  );
 }
--- frontend/components/Suggestions.jsx
+++ frontend/components/Suggestions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Suggestions() {
-  return <div>Suggestions</div>;
+  return <div className="">Suggestions</div>;
 }
